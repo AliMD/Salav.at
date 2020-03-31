4eb2125460deb8f99b301e0cbdf611e10d9f64ae
Ali Mihandoost <alimihandoost@gmail.com>
2020-04-01 01:40:17 +0430

    refactor(yarn): scripts and build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,6 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const replace = require('gulp-replace');
-const del = require('del');
-const spawn = require('child_process').spawn;
-
-/**
- * Cleans the build build in the server directory.
- */
-gulp.task('build:clean', () => {
-  return del('dist');
-});
 
 /**
  * Copies the build build to the server directory while renaming the
@@ -18,8 +9,6 @@ gulp.task('build:clean', () => {
 gulp.task('build:asset', () => {
   const pattern = 'node_modules';
   const replacement = 'asset';
-
-  // move robot.txt to root dist
 
   return gulp.src('build/**')
     .pipe(rename(((path) => {
@@ -31,20 +20,5 @@ gulp.task('build:asset', () => {
 });
 
 gulp.task('build', gulp.series(
-  'build:clean',
   'build:asset'
 ));
-
-/**
- * Gulp task to run `tsc --watch` and `polymer serve` in parallel.
- */
-gulp.task('serve', () => {
-  const spawnOptions = {
-    // `shell` option for Windows compatability. See:
-    // https://nodejs.org/api/child_process.html#child_process_spawning_bat_and_cmd_files_on_windows
-    shell: true,
-    stdio: 'inherit'
-  };
-  spawn('tsc', ['--watch'], spawnOptions);
-  spawn('polymer', ['serve'], spawnOptions);
-});
