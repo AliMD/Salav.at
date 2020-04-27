d43dbe100de725e0cea78f33c1f08162bb322104
Ali Mihandoost <alimihandoost@gmail.com>
2020-04-27 14:59:00 +0430

    release: v1.2.4

--- src/config.ts
+++ src/config.ts
@@ -72,7 +72,7 @@ export const pageListArray: Array<MenuItem | MenuItemHidden> = [
 ]
 
 export const appConfig = {
-  appVersion: '1.2.3',
+  appVersion: '1.2.4',
   apiUri: 'https://api.salav.at',
   apiToken: 'pazh-vxah4f79o2ir97evva9ts7p5ya94zyx2-fjt',
   apiSalavatCountDocId: 'salavat/count',
