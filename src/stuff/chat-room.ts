3516b956c48169a686a9b4e622533cf1cc04cbe6
Mohammad Honarvar <30275117+MohammadHonarvar@users.noreply.github.com>
2020-05-10 23:36:48 +0430

    feat(chat-room): using new custom debounce

--- src/stuff/chat-room.ts
+++ src/stuff/chat-room.ts
@@ -1,35 +1,32 @@
-import { AsyncInterface } from '@polymer/polymer/interfaces';
-import { Debouncer } from '@polymer/polymer/lib/utils/debounce';
-import { animationFrame } from '@polymer/polymer/lib/utils/async';
+import { isRepeated } from './debounce';
 
-// export const eventTarget: EventTarget = 'EventTarget' in window ? new EventTarget() : document.createElement('span');
-export const eventTarget: EventTarget = document.createElement('span');
+export const eventTarget: EventTarget = 'EventTarget' in window ? new EventTarget() : document.createElement('span');
 const dispatchEventHistory: Record<string, unknown> = {};
-const dispatchJobList: Record<string, Debouncer> = {};
-const debug = true;
 
 const _log = (message: unknown, ...restParam: unknown[]) => {
-  if (debug) {
-    console.log(`%cChatRoom%c ${message}`, "color: #4CAF50; font-size: 1.2em;", "color: inherit;font-size: 1em", ...restParam);
+  if (window['pazhDebug7112']) {
+    console.log(`%cAppRoom%c ${message}`, "color: #4CAF50; font-size: 1.2em;", "color: inherit;font-size: 1em", ...restParam);
   }
 };
 
 const onMessage = (eventName: string, callback: (detail?: unknown) => void, option: { preserve: boolean } = { preserve: true }) => {
-  _log('onMessage: %s %s', eventName, option.preserve ? '(preserve)' : '');
+  _log('onMessage on %s %s', eventName, option.preserve ? '(preserve)' : '');
   eventTarget.addEventListener(eventName, (event: Event) => callback(event['detail']));
-  if (option.preserve && eventName in dispatchEventHistory) {
+  if (option.preserve && dispatchEventHistory[eventName] !== undefined) {
     setTimeout(callback, 0, dispatchEventHistory[eventName]); // callback in next micro task
   }
 };
 
-const postMessage = (eventName: string, detail?: string | boolean | unknown, option: { preserve: boolean } = { preserve: true }) => {
+const postMessage = async (eventName: string, detail?: string | boolean | unknown, option: { preserve: boolean } = { preserve: true }) => {
   if (option.preserve) {
     dispatchEventHistory[eventName] = detail;
   }
-  dispatchJobList[eventName] = Debouncer.debounce(dispatchJobList[eventName], animationFrame as AsyncInterface, () => {
-    _log('postMessage: %s with %o %s', eventName, detail, option.preserve ? '(preserve)' : '');
-    eventTarget.dispatchEvent(new CustomEvent(eventName, { detail }));
-  });
+
+  if (await isRepeated(eventName)) return;
+
+  detail = dispatchEventHistory[eventName]; // get last changed detail value
+  _log('postMessage %s with %o %s', eventName, detail, option.preserve ? '(preserve)' : '');
+  eventTarget.dispatchEvent(new CustomEvent(eventName, { detail }));
 };
 
 const setProperty = (propertyName: string, newValue: unknown) => {
@@ -45,4 +42,4 @@ const onPropertyChanged = (propertyName: string, callback: (newVal?: unknown) =>
   onMessage(`appProperty_${propertyName}_changed`, callback, { preserve: true });
 };
 
-export const chatRoom = {onMessage, postMessage, setProperty, getProperty, onPropertyChanged};
+export const appRoom = {onMessage, postMessage, setProperty, getProperty, onPropertyChanged};
