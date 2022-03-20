0dd5dce1f2c2e4d8c06ee5c2dc0bfc3b6a7fa895
Amir Najafi <njfamirm@gmail.com>
2022-03-21 01:37:24 +0330

    fix(logger): remove logger from baseElemnt class

--- src/stuff/base-element.ts
+++ src/stuff/base-element.ts
@@ -1,7 +1,6 @@
 import {LitElement, property} from 'lit-element';
-import {appConfig} from '../config';
+import {appConfig, Logger} from '../config';
 
-type consoleLogLevel = 'log' | 'info' | 'clear' | 'debug' | 'error' | 'warn' | 'trace'
 
 export abstract class BaseElement extends LitElement {
   @property({type: Boolean, reflect: true}) debug = appConfig.debug;
@@ -11,35 +10,8 @@ export abstract class BaseElement extends LitElement {
     super.performUpdate();
   }
 
-  private __logger(logLevel: consoleLogLevel, message: unknown, ...restParam: unknown[]):void {
-    // first args must be separated as keyPattern for fix issue of `this._log('a=%s', a)`
-    const tagName = (this.tagName + '').toLowerCase();
-    console[logLevel](
-        `%c<%s>%c ${message}`, 'color: #4CAF50; font-size: 1.2em;',
-        tagName, 'color: inherit;font-size: 1em', ...restParam,
-    );
-  }
-
-  protected log(message: unknown, ...restParam: unknown[]):void {
-    this.__logger('log', message, ...restParam);
-  }
-
-  protected _log(message: unknown, ...restParam: unknown[]):void {
-    if (this.debug) {
-      this.log(message, ...restParam);
-    }
-  }
-
-  protected _warn(message: unknown, ...restParam: unknown[]):void {
-    this.__logger('warn', message, ...restParam);
-  }
-
-  protected _error(message: unknown, ...restParam: unknown[]):void {
-    this.__logger('error', message, ...restParam);
-  }
-
   protected _fire(eventName: string, detail: unknown, bubbles = false):void {
-    this._log('fire %s {%o}', eventName, detail);
+    Logger.incident('event', 'fire_event', 'fire %s {%o}', eventName, detail);
     this.dispatchEvent(new CustomEvent(eventName, {
       detail,
       bubbles,
